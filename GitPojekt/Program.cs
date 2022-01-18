b812ce94db5f82dd31e5012b7efe902cd9d41603
Matěj Plavina <m.plavina@student.nosch.cz>
2022-01-18 12:48:43 +0100

    Přidejte soubory projektu.

---
+++ GitPojekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitPojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
